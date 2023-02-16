72374d1d39dbc0eef8a5afab9d5715230373decc
Idan David <idand2000@gmail.com>
2023-02-16 17:46:21 +0200

    add list and preview cmp

---
+++ src/app/cmps/who-watch-preview/who-watch-preview.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'who-watch-preview',
+  templateUrl: './who-watch-preview.component.html',
+  styleUrls: ['./who-watch-preview.component.scss']
+})
+export class WhoWatchPreviewComponent {
+
+}
