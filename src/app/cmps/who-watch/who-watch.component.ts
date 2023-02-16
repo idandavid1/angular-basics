77b991b97e2f63fa81429166b79f2c4244c9b137
ofer <ofergavri@gmail.com>
2023-02-16 17:46:51 +0200

    added styles

--- src/app/cmps/who-watch/who-watch.component.ts
+++ src/app/cmps/who-watch/who-watch.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from 'src/app/modules/user.module';
 
 @Component({
   selector: 'who-watch',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./who-watch.component.scss']
 })
 export class WhoWatchComponent {
+  users!: User[]
 
+  ngOnInit(): void {
+    this.users = [
+      { _id: "101", name: "Brian", imgUrl: "imgs/profile-1.png" },
+      { _id: "102", name: "Ofer", imgUrl: "imgs/profile-2.png" },
+      { _id: "103", name: "Dan", imgUrl: "imgs/profile-3.png" },
+      { _id: "104", name: "Gil", imgUrl: "imgs/profile-4.png" },
+    ]
+  }
 }
---
+++ src/app/modules/user.module.ts
@@ -0,0 +1,5 @@
+export interface User {
+    name: string
+    _id: string
+    imgUrl: string
+}
